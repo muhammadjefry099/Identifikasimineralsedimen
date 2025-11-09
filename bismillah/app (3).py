cf6ebea5aa42d6b50c33e467cdba8b9deaebea98
muhammadjefry099 <muhammadjefry099@gmail.com>
2025-11-09 19:38:48 +0800

    Update app (3).py

--- bismillah/app (3).py
+++ bismillah/app (3).py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 
 # Load the trained YOLOv8 model
 # Make sure the model file exists in the correct path
-model = YOLO('/content/best (4).pt')
+model = YOLO('best (4).pt')
 
 st.title("Deteksi Objek dengan YOLOv8")
 
